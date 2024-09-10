993ca23cecefb631dfe24d7da423fdbbc873c675
pnquynhdev <pnquynh.dev@gmail.com>
2024-09-11 01:22:52 +0700

    copy from html to html in react project

--- GumacCloneReact/src/App.jsx
+++ GumacCloneReact/src/App.jsx
@@ -1,10 +1,11 @@
 import styles from './App.module.css'
+import Nav from './navbar/Nav';
 
 function App() {
 
   return (
     <div className={styles.App}>
-      
+      <Nav/>
     </div>
   )
 }
--- GumacCloneReact/src/navbar/Nav.jsx
+++ GumacCloneReact/src/navbar/Nav.jsx
@@ -3,7 +3,48 @@ import React from "react";
 import { getImageUrl } from "../utils";
 
 const Nav = () => {
-  return <div></div>;
+  return (
+    <nav>
+      <div className="navbar">
+        
+          <a className="logo" href="/home">
+            <img
+              src={getImageUrl("navbar/header_logo.svg")}
+              alt="header_logo"
+            />
+          </a>
+        
+
+        <div className="search_box">
+          <input type="text" placeholder="Search product" />
+          <button>
+            <img
+              src={getImageUrl("navbar/search_icon.svg")}
+              alt="search_icon"
+            />
+          </button>
+        </div>
+        <ul className="links">
+          <li>
+            <a href="/collection">Collection</a>
+          </li>
+          <li>
+            <a href="/product">Product</a>
+          </li>
+          <li>
+            <a href="/New">New</a>
+          </li>
+        </ul>
+
+        <a className="client_info" href="/client">
+          <img src={getImageUrl("navbar/user_icon.svg")} alt="user_icon" />
+        </a>
+        <div className="toggle_btn">
+          <img src={getImageUrl("navbar/toggle_btn.svg")} alt="toggle_btn" />
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
