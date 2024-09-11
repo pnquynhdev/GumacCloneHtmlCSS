dbc624485ea188c0f9b1b2b784e97aaccef456b1
pnquynhdev <pnquynh.dev@gmail.com>
2024-09-12 00:25:34 +0700

    Done responsive Navbar

--- GumacCloneReact/src/navbar/Nav.jsx
+++ GumacCloneReact/src/navbar/Nav.jsx
@@ -1,8 +1,9 @@
 import styles from "./Nav.module.css";
-import React from "react";
+import React, { useState } from "react";
 import { getImageUrl } from "../utils";
 
 const Nav = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <header>
       <nav>
@@ -37,34 +38,48 @@ const Nav = () => {
             <a className={styles.client_info} href="/client">
               <img src={getImageUrl("navbar/user_icon.svg")} alt="user_icon" />
             </a>
+            <div className={styles.toggle_btn}>
+              <img
+                src={
+                  menuOpen
+                    ? getImageUrl("navbar/close_btn.svg")
+                    : getImageUrl("navbar/toggle_btn.svg")
+                }
+                alt="toggle_btn"
+                onClick={() => setMenuOpen(!menuOpen)}
+              />
+            </div>
           </div>
-          <div className={styles.toggle_btn}>
-            <img src={getImageUrl("navbar/toggle_btn.svg")} alt="toggle_btn" />
+        </div>
+
+        {/* DROPDOWN */}
+        <div
+          className={`${styles.dropdown_menu_close} ${
+            menuOpen && styles.dropdown_menu
+          }`}
+        >
+          <div className={styles.dropdown_searchbox}>
+            <input type="text" placeholder="Search product" />
+            <button>
+              <img
+                src={getImageUrl("navbar/search_icon.svg")}
+                alt="search_icon"
+              />
+            </button>
           </div>
+          <ul className={styles.dropdown_links}>
+            <li>
+              <a href="/collection">Collection</a>
+            </li>
+            <li>
+              <a href="/product">Product</a>
+            </li>
+            <li>
+              <a href="/New">New</a>
+            </li>
+          </ul>
         </div>
       </nav>
-
-      {/* DROPDOWN MENU */}
-      <div className={styles.dropdown_menu}>
-        <div className={styles.dropdown_searchbox}>
-          <input type="text" placeholder="Search product" />
-          <button>
-            <img
-              src={getImageUrl("navbar/search_icon.svg")}
-              alt="search_icon"
-            />
-          </button>
-        </div>
-        <li>
-          <a href="/collection">Collection</a>
-        </li>
-        <li>
-          <a href="/product">Product</a>
-        </li>
-        <li>
-          <a href="/New">New</a>
-        </li>
-      </div>
     </header>
   );
 };
