d95f9ef8e67d066af4cbe8ead3c9f3704a15f71f
pnquynhdev <pnquynh.dev@gmail.com>
2024-09-15 02:04:31 +0700

    add react to banner

--- GumacCloneReact/src/banner/Banner.jsx
+++ GumacCloneReact/src/banner/Banner.jsx
@@ -1,8 +1,42 @@
-import React from "react";
+import { useState } from "react";
 import styles from "./Banner.module.css";
 import { getImageUrl } from "../utils";
+import { ChevronLeft, ChevronRight } from "react-feather";
 
 const Banner = () => {
+  const [currentImg, setCurrentImg] = useState(0);
+  const carouselImgs = [
+    {
+      id: 0,
+      link: "banner/carousel_1.jpg",
+      alt: "carousel_1",
+    },
+    {
+      id: 1,
+      link: "banner/carousel_2.jpg",
+      alt: "carousel_2",
+    },
+    {
+      id: 2,
+      link: "banner/carousel_3.jpg",
+      alt: "carousel_3",
+    },
+  ];
+
+  const prevImg = () => {
+    setCurrentImg((currentImg) =>
+      currentImg === 0 ? carouselImgs.length - 1 : currentImg - 1
+    );
+    console.log(currentImg);
+  };
+
+  const nextImg = () => {
+    setCurrentImg((currentImg) =>
+      currentImg === carouselImgs.length - 1 ? 0 : currentImg + 1
+    );
+    console.log(currentImg);
+  };
+
   return (
     <section>
       <div className={styles.banner_container}>
@@ -27,35 +61,40 @@ const Banner = () => {
               />
               <div className={styles.voucher_wrapper}>
                 <div className={styles.code}></div>
-              <div className={styles.voucher_content}>
-                <h1>VOUCHER 20k</h1>
-                <p>Giảm 20k cho đơn từ 350k</p>
-                <button>COPY MÃ</button>
-              </div></div>
+                <div className={styles.voucher_content}>
+                  <h1>VOUCHER 20k</h1>
+                  <p>Giảm 20k cho đơn từ 350k</p>
+                  <button>COPY MÃ</button>
+                </div>
+              </div>
             </div>
             <div className={styles.voucher_code}>
               <img
                 src={getImageUrl("banner/voucher_code.png")}
                 alt="voucher_image"
-              /><div className={styles.voucher_wrapper}>
+              />
+              <div className={styles.voucher_wrapper}>
                 <div className={styles.code}></div>
-              <div className={styles.voucher_content}>
-                <h1>VOUCHER 20k</h1>
-                <p>Giảm 20k cho đơn từ 350k</p>
-                <button>COPY MÃ</button>{" "}
-              </div></div>
+                <div className={styles.voucher_content}>
+                  <h1>VOUCHER 20k</h1>
+                  <p>Giảm 20k cho đơn từ 350k</p>
+                  <button>COPY MÃ</button>{" "}
+                </div>
+              </div>
             </div>
           </div>
         </div>
         <div className={styles.carousel}>
           <div className={styles.carousel_items}>
-            <div className={styles.carousel_item}>
-              <img
-                src={getImageUrl("banner/carousel_1.jpg")}
-                alt="carousel_1"
-              />
-            </div>
-            <div className={styles.carousel_item_selected}>
+            {carouselImgs.map((carouselImg) => (
+              <div key={carouselImg.id} className={styles.carousel_item}>
+                <img
+                  src={getImageUrl(carouselImg.link)}
+                  alt={carouselImg.alt}
+                />
+              </div>
+            ))}
+            {/* <div className={styles.carousel_item_selected}>
               <img
                 src={getImageUrl("banner/carousel_2.jpg")}
                 alt="carousel_2"
@@ -66,8 +105,14 @@ const Banner = () => {
                 src={getImageUrl("banner/carousel_3.jpg")}
                 alt="carousel_3"
               />
-            </div>
+            </div> */}
           </div>
+          <button className={styles.carousel_button} onClick={prevImg}>
+            <ChevronLeft />
+          </button>
+          <button className={styles.carousel_button} onClick={nextImg}>
+            <ChevronRight />
+          </button>
           <div className={styles.carousel_slick_dots}>
             <span></span>
             <span className={styles.selected}></span>
